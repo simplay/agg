e3a46d89e6cd23cdc717e50fed25bd8304d82e55
simplay <silent.simplayy@gmail.com>
2020-04-03 20:42:38 +0200

    Define supersimple jump mechanic

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    //public Rigidbody rg;
+    public Rigidbody rg;
 
-    public float walkingSpeed = 0.5f;
-    public float runningSpeed = 1f;
+    public float walkingSpeed = 0.2f;
+    public float runningSpeed = 0.4f;
 
     private bool isRunning = false;
     private bool isMovingForward = false;
@@ -15,6 +15,7 @@ public class PlayerMovement : MonoBehaviour
     private bool isMovingLeft = false;
     private bool isMovingRight = false;
     private bool jump = false;
+    private bool onGround = true;
 
     private GameManager gm;
 
@@ -22,6 +23,14 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         gm = GameObject.FindObjectOfType<GameManager>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            onGround = true;
+        }
     }
 
     // Update is called once per frame
@@ -72,11 +81,13 @@ public class PlayerMovement : MonoBehaviour
         if (isMovingForward)
         {
             transform.position += transform.TransformDirection(Vector3.forward * speed);
+            // rg.velocity = transform.TransformDirection(Vector3.forward * speed);
         }
 
         if (isMovingBackward)
         {
             transform.position += transform.TransformDirection(Vector3.back * speed);
+            // rg.velocity = transform.TransformDirection(Vector3.back * speed);
         }
 
         if (isMovingLeft)
@@ -89,9 +100,10 @@ public class PlayerMovement : MonoBehaviour
             transform.position += transform.TransformDirection(Vector3.right * speed * 0.75f);
         }
 
-        if (jump)
+        if (jump && onGround)
         {
-            
+            onGround = false;
+            rg.AddForce(Vector3.up * 8f, ForceMode.VelocityChange);
         }
     }
 }
