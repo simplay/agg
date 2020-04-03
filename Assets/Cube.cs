04acccede2841981c994199fe73ca4e6a865f9c5
simplay <silent.simplayy@gmail.com>
2020-04-03 21:28:13 +0200

    Play placement sound

---
+++ Assets/Cube.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioSource audioSource = GetComponent<AudioSource>();
+        Debug.Log(audioSource.clip.name);
+        audioSource.Play();
+    }
+}
